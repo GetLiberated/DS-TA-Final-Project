089f69315630aad2265c29530729b68cf26369ea
GetLiberatedDev <43334192+GetLiberatedDev@users.noreply.github.com>
2020-01-20 13:08:17 +0700

    Merged login page

---
+++ home.php
@@ -0,0 +1,119 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>MySQL Final Project</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body {
+            background-image: url("BGImage.jpg");
+            padding: 25px;
+            background-repeat: no-repeat;
+            background-size: auto;
+            
+        }
+/* filter: saturate(3); 
+filter: grayscale(200%); 
+filter: contrast(160%); 
+filter: brightness(0.25); 
+filter: blur(4px); 
+filter: invert(100%); 
+filter: sepia(100%); 
+filter: hue-rotate(180deg); 
+filter: opacity(40%);  */
+            /*Filter styles*/
+
+        }
+
+        .Menu {
+            height: 100%;
+            color: blue;
+            text-align: center;
+            display: table;
+            margin: auto;
+            display: table-cell;
+            vertical-align: middle; 
+            position: relative;
+            left: 1000x;
+            /* /* border: 3px solid #73AD21; */
+        }
+        a {
+            background-color: #008000;
+            /* float:center; */
+            color: white;
+            padding: 14px 25px;
+            text-align: center; 
+            text-decoration: none;
+            display: inline-block;
+            font-weight: bold;
+            font-family: monospace;
+            border-radius: 5px;
+            font-size: 20px;
+            margin: 20px 20px;
+            position: relative;
+            left: 410px;
+            border: 1px solid #73AD21;
+        }
+
+        a:hover, a:active {
+            background-color: #006400;
+        }
+        
+        h1 {
+            display:inline-block;
+            color : black;
+            text-align: left;
+            font-weight: bold;
+            font-family: ourFond;
+            font-size: 80px;
+            margin: 7.5px 10px;
+        }
+        h2{
+            float:right;
+            display:inline-block;
+            font-family : ourFond;
+            color : #228B22;
+            text-align: center;
+            font-weight: bold;
+            font-size:50px;
+            border-radius: 5px;
+            margin: 7.5px 10px;
+            
+
+        }
+        #icon i{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    
+    <!-- <img src="BGImage.jpg" alt="BackGroundImage" height = "1280" widght = "720"> -->
+    <h1>Database TA</h1><h2>WINGSTOP</h2> 
+    <div id= "icon" class="w3-padding w3-xlarge w3-teal">
+        <i class="fa fa-bars" ></i>
+        <i class="fa fa-close"></i>
+        <i class="fa fa-arrow-left"></i>
+        <i class="fa fa-search"></i>
+        <i class="fa fa-refresh"></i>
+        <i class="fa fa-arrow-right"></i>
+
+
+    </div>
+    <div class = "Menu"> 
+        <br><br><br><br><br><br><br>
+        <a href="address/address.php">Address</a>
+        <a href="transaction/transaction.php">Transaction</a>
+        <a href="restaurant/restaurant.php">Restaurant</a>
+        <br>
+        <a href="staff/staff.php">Staff</a>
+        <a href="Item/item.php">Item</a>
+        <a href="transactionDetail/transactionDetail.php">Transaction Detail</a>
+        <br><br><br>
+        <p><i class="w3-jumbo w3-spin fa fa-home"></i></p>
+        
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,119 +1,67 @@
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <title>MySQL Final Project</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        body {
-            background-image: url("BGImage.jpg");
-            padding: 25px;
-            background-repeat: no-repeat;
-            background-size: auto;
-            
-        }
-/* filter: saturate(3); 
-filter: grayscale(200%); 
-filter: contrast(160%); 
-filter: brightness(0.25); 
-filter: blur(4px); 
-filter: invert(100%); 
-filter: sepia(100%); 
-filter: hue-rotate(180deg); 
-filter: opacity(40%);  */
-            /*Filter styles*/
+	<title>Admin page</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        }
 
-        .Menu {
-            height: 100%;
-            color: blue;
-            text-align: center;
-            display: table;
-            margin: auto;
-            display: table-cell;
-            vertical-align: middle; 
-            position: relative;
-            left: 1000x;
-            /* /* border: 3px solid #73AD21; */
-        }
-        a {
-            background-color: #008000;
-            /* float:center; */
-            color: white;
-            padding: 14px 25px;
-            text-align: center; 
-            text-decoration: none;
-            display: inline-block;
-            font-weight: bold;
-            font-family: monospace;
-            border-radius: 5px;
-            font-size: 20px;
-            margin: 20px 20px;
-            position: relative;
-            left: 410px;
-            border: 1px solid #73AD21;
-        }
+	<link rel="stylesheet" href="main.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script scd cdrc="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/smoothScroll.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
+	<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+	<!-- Bootstrap Date-Picker Plugin -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+	<style type="text/css">
+	.video-container {
+		position: relative;
+	}
 
-        a:hover, a:active {
-            background-color: #006400;
-        }
-        
-        h1 {
-            display:inline-block;
-            color : black;
-            text-align: left;
-            font-weight: bold;
-            font-family: ourFond;
-            font-size: 80px;
-            margin: 7.5px 10px;
-        }
-        h2{
-            float:right;
-            display:inline-block;
-            font-family : ourFond;
-            color : #228B22;
-            text-align: center;
-            font-weight: bold;
-            font-size:50px;
-            border-radius: 5px;
-            margin: 7.5px 10px;
-            
-
-        }
-        #icon i{
-            color: red;
-        }
-    </style>
+	.overlay-desc {
+		background: rgba(0,0,0,0);
+		position: absolute;
+		top: 0; right: 0; bottom: 0; left: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 100px
+	}
+	.header-link{
+		color: black
+	}
+	.header-link:hover{
+		color: white
+	}
+	</style>
 </head>
-<body>
-    
-    <!-- <img src="BGImage.jpg" alt="BackGroundImage" height = "1280" widght = "720"> -->
-    <h1>Database TA</h1><h2>WINGSTOP</h2> 
-    <div id= "icon" class="w3-padding w3-xlarge w3-teal">
-        <i class="fa fa-bars" ></i>
-        <i class="fa fa-close"></i>
-        <i class="fa fa-arrow-left"></i>
-        <i class="fa fa-search"></i>
-        <i class="fa fa-refresh"></i>
-        <i class="fa fa-arrow-right"></i>
+<body style="background-color: white">
+	<img style="width: 100%;opacity: 0.2" src="assets/logo1.jpg" alt="">
+	<div class="overlay-desc" style="margin-top: -210px">
+		<h4>Admin Login</h4>
+	</div>
+	<div class="overlay-desc">
+		<form action="" method="post">
+			<div class="form-group">
+				<label for="exampleInputEmail1"></label>
+				<input type="text" name="Name" class="form-control" placeholder="username" id="exampleInputEmail1" aria-describedby="emailHelp">
+			</div>
+			<div class="form-group">
+				<input type="password" name="Pass" class="form-control" placeholder="password" id="exampleInputPassword1">
+			</div>
+			<button name="admin_login" style="width: 100%" type="submit" class="btn btn-primary">login</button>
+		</form>
+	</div>
+</div>
 
-
-    </div>
-    <div class = "Menu"> 
-        <br><br><br><br><br><br><br>
-        <a href="address/address.php">Address</a>
-        <a href="transaction/transaction.php">Transaction</a>
-        <a href="restaurant/restaurant.php">Restaurant</a>
-        <br>
-        <a href="staff/staff.php">Staff</a>
-        <a href="Item/item.php">Item</a>
-        <a href="transactionDetail/transactionDetail.php">Transaction Detail</a>
-        <br><br><br>
-        <p><i class="w3-jumbo w3-spin fa fa-home"></i></p>
-        
-    </div>
-</body>
-</html>
+<script>
+	window.onload = function() {
+    // similar behavior as clicking on a link
+    window.location.href = "home.php";
+	}
+</script>
