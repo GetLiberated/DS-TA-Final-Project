43008d9128d9b62086fc32f45a9d2a0c56470b92
livander <livandersurya321@gmail.com>
2020-01-21 19:14:55 +0700

    Create checkout.php

---
+++ Checkout/checkout.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="dist/jquery.tabledit.js"></script>
+    <style>
+        body {  
+            background: linear-gradient(rgba(255,255,255,.8), rgba(255,255,255,.8)), url("logo1.jpg");
+            background-repeat: no-repeat;
+            background-size: auto;
+            background-position:top left;         
+        } 
+        @font-face {
+            font-family: ourFond;
+            src: url(LobsterTwo-Regular.ttf);
+            font-weight: bold;
+        }
+        .back{
+            border-radius : 10px;
+            display: inline-block;
+        }
+    </style>
+    
+</head>
+<body>
+<p id="debug"></p>
+    <div class = "back">
+        <a href="../New Order/New Order.php"><i class="fa fa-arrow-left"></i></a>    
+    </div>
+<!-- <a href="address/address.php">Address</a> -->
+</body>
+</html>
